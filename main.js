a0d4f84089bf43b53aefd7094fa87700949d4cbf
ChiragThapa <chiragThapa777@gmail.com>
2022-03-09 22:22:11 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+let cells=document.querySelectorAll(".cell")
+cells=Array.from(cells)
+
+let winText=document.getElementById("winText")
+winText.addEventListener("click",()=>{
+    location.reload()
+    
+})
+
+let currentPlayer="X"
+
+let winCombo=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+const checkForWinner=()=>{
+    winCombo.forEach((combo)=>{
+        let check =combo.every(index=>cells[index].innerText.trim()==currentPlayer)
+        if(check){
+            highlightCells(combo)
+        }
+    })
+}
+
+const highlightCells=(combo)=>{
+    combo.forEach(index=>{
+        cells[index].classList.add("highlight")
+    })
+    winText.innerText=`${currentPlayer} Wins!!! Click here to restart`
+}
+cells.forEach((cell)=>{
+    cell.addEventListener('click',()=>{
+        if(cell.innerText.trim()!="")return
+        cell.innerText=currentPlayer
+        checkForWinner()
+        currentPlayer=currentPlayer=="X"?"O":"X"
+    })
+})
